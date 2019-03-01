9c6aad8900c669c8e3f990bb795ea5f9866235c7
AlexGLovett <alexanderglovett@gmail.com>
2019-02-28 20:15:45 -0500

    added html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,1 +1,26 @@
-// html routes HERE (refer to week 15 activities for examples, connect to handlebars files in /views folder)
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  // index route loads view.html
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/index.handlebars"));
+  });
+
+  // cms route loads cms.html
+  app.get("/dash", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/dashboard.handlebars"));
+  });
+
+  // blog route loads blog.html
+  app.get("/make", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/createdash.handlebars"));
+  });
+
+}; 
