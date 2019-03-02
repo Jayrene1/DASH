0273e1817590fbc8268910111e36f03496d04c46
alexglovett <alexanderglovett@gmail.com>
2019-03-02 12:28:35 -0500

    product, sector, and country selectors added

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -15,3 +15,53 @@ module.exports = function(app) {
     });
   };
   
+    // Get route for retrieving a single post
+    app.get("/api/samples/:id", function(req, res) {
+      // 2. Add a join here to include the Author who wrote the Post
+      db.sample.findOne({
+        where: {
+          id: req.params.id
+        }
+      }).then(function(result) {
+        console.log(result);
+        res.json(result);
+      });
+    });
+
+    // Get route for retrieving a single post
+    app.get("/api/samples/:Country", function(req, res) {
+      // 2. Add a join here to include the Author who wrote the Post
+      db.sample.findOne({
+        where: {
+          country: req.params.country
+        }
+      }).then(function(result) {
+        console.log(result);
+        res.json(result);
+      });
+    });
+
+    // Get route for retrieving a single post
+    app.get("/api/samples/:Sector", function(req, res) {
+      // 2. Add a join here to include the Author who wrote the Post
+      db.sample.findOne({
+        where: {
+          sector: req.params.sector
+        }
+      }).then(function(result) {
+        console.log(result);
+        res.json(result);
+      });
+    });
+    // Get route for retrieving a single post
+    app.get("/api/samples/:Product", function(req, res) {
+      // 2. Add a join here to include the Author who wrote the Post
+      db.sample.findOne({
+        where: {
+          product: req.params.product
+        }
+      }).then(function(result) {
+        console.log(result);
+        res.json(result);
+      });
+    });
