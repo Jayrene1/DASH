eabe51a5fcc2fd29f296d117573c6e8ad47d7420
Jayrene1 <43413926+Jayrene1@users.noreply.github.com>
2019-03-08 18:13:09 -0500

    fixed associations and added empty files for separating api routes by model

--- models/graphs.js
+++ models/graphs.js
@@ -1,12 +1,24 @@
 module.exports = function(sequelize, DataTypes) {
-	return sequelize.define('graphs', {
+	var Graph = sequelize.define('graphs', {
 		graph_name: {
 			type: DataTypes.STRING(24),
 			allowNull: false
 		},
         graph_values: {
-			type: DataTypes.STRING(255),
-			allowNull: true
+			type: DataTypes.JSON,
+			allowNull: false
 		}
 	});
+
+	Graph.associate = function(models) {
+		// We're saying that a Post should belong to an Author
+		// A Post can't be created without an Author due to the foreign key constraint
+		Graph.belongsTo(models.dashboards, {
+		  foreignKey: {
+			allowNull: false
+		  }
+		});
+	};
+
+	return Graph;
 };
